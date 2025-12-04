887180a26c40845519b24dc0be7d229aa718426a
liop92 <gheorghe.cristian92@gmail.com>
2025-12-04 13:59:41 +0200

    Types

---
+++ 02_types.py
@@ -0,0 +1,11 @@
+name = "Anna"
+age = 27
+height_meters = 1.77
+female = True
+favorite_color = "Purple"
+
+print(type(name))
+print(type(age))
+print(type(height_meters))
+print(type(female))
+print(type(favorite_color))
