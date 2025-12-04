0d82ac3a3c74f85f13ee43c9dcb8c728d8225ca2
liop92 <gheorghe.cristian92@gmail.com>
2025-12-04 14:06:35 +0200

    Input

---
+++ 03_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+city = input("What city do you live in?: ")
+
+print(f"Hello, {name}! You are {age} years and you live in {city}.")
